00e8254d9daffe1b34bfe43f70e5f25408b491b7
PraveenNegi1 <praveennegi700@gmail.com>
2025-05-24 16:03:14 +0530

    feat: Update Footer and Navbar components with new background color for improved aesthetics

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-teal-900 to-teal-800 text-white py-12">
+    <footer className=" bg-[#30434c] text-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 font-serif">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
           <div className="flex flex-col items-center md:items-start">

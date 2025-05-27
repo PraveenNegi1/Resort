a8effb5dca47d30a26ed23112286603955956795
PraveenNegi1 <praveennegi700@gmail.com>
2025-05-27 10:46:01 +0530

    feat: Remove Heropage and QuoteCard components to streamline homepage layout

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import Hero from "@/components/Hero";
 import LaptopVideo from "@/components/Laptop";
 import Services from "@/components/Services";
 import DiningSection from "@/components/Dishes";
-import Heropage from "@/components/Brands";
-import QuoteCard from "@/components/QuoteCard";
+
+
 
 export default function Home() {
   return (
@@ -16,8 +16,8 @@ export default function Home() {
       <Services />
       <LaptopVideo />
       <DiningSection />
-      <Heropage />
-      <QuoteCard />
+
+
     </>
   );
 }
